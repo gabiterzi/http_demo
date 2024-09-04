7f45941f3da8547eb4adc00a6d9a415181c60aba
gabiterzi <labsfiap@fiap.com.br>
2024-09-03 22:18:04 -0300

    deixando o id dinâmico

--- bin/http_demo2.dart
+++ bin/http_demo2.dart
@@ -3,13 +3,14 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 void main(List<String> arguments) async{
   final response = await getTodo();
-  print(response);
+  print(response['title']);
+  print(response['completed']);
 }
 
-Future<String> getTodo() async{
+Future<Map> getTodo() async{
   final url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
   final response = await http.get(url);
   final Map map = jsonDecode(response.body);
   //Map serve para reconhecer o nome dos campos do json
-  return map['title'];
+  return map;
 }
