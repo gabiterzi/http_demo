edde80e5522eccb0fd845a9a39dc85f979a7e4e7
gabiterzi <labsfiap@fiap.com.br>
2024-09-03 22:26:54 -0300

    aprimorando o método getTodo

--- bin/http_demo2.dart
+++ bin/http_demo2.dart
@@ -2,15 +2,30 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 void main(List<String> arguments) async{
-  final response = await getTodo();
-  print(response['title']);
-  print(response['completed']);
+  final response = await getTodo(1);
+  print(response.title);
+  print(response.completed);
 }
 
-Future<Map> getTodo() async{
-  final url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
+Future<Todo> getTodo(int id) async{
+  final url = Uri.parse('https://jsonplaceholder.typicode.com/todos/$id');
   final response = await http.get(url);
-  final Map map = jsonDecode(response.body);
+  final Map<String, dynamic> map= jsonDecode(response.body);
   //Map serve para reconhecer o nome dos campos do json
-  return map;
+  return Todo(
+    id: map['id'],
+    userId: map['userId'],
+    title: map['title'],
+    completed: map['completed'],
+  );
+}
+
+
+class Todo {
+  final int id;
+  final int userId;
+  final String title;
+  final bool completed;
+
+  Todo({required this.id, required this.userId, required this.title, required this.completed});
 }
