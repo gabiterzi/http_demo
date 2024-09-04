29437053f02cf55fa59bf31c870fd35c800aff94
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-03 22:11:06 -0300

    Alterar método getTodo e usar Map

--- bin/http_demo2.dart
+++ bin/http_demo2.dart
@@ -1,11 +1,15 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 void main(List<String> arguments) async{
-  getTodo();
+  final response = await getTodo();
+  print(response);
 }
 
-void getTodo() async{
+Future<String> getTodo() async{
   final url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
   final response = await http.get(url);
-  print(response.statusCode);
-  print(response.body);
+  final Map map = jsonDecode(response.body);
+  //Map serve para reconhecer o nome dos campos do json
+  return map['title'];
 }
